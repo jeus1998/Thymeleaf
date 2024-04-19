dc8d1ead358117d55045fd3f2f743caf769de687
baejeu12 <baejeu@naver.com>
2024-04-20 03:23:53 +0900

    first commit

---
+++ src/main/java/Thymeleaf/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package Thymeleaf.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Thymeleaf/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package Thymeleaf.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
